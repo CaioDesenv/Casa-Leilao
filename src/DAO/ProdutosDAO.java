929669704b03472562ffb54f4351c90fc8ed0dac
Caio Vinicius <caiolopes0800@gmail.com>
2023-09-23 19:25:43 -0300

    Botão Cadastrar funcinando com JDBC e com msg de conclusão

--- src/DAO/ProdutosDAO.java
+++ src/DAO/ProdutosDAO.java
@@ -1,17 +1,8 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
-
-/**
- *
- * @author Adm
- */
+package DAO;
 
 import Controller.ProdutosDTO;
 import java.sql.PreparedStatement;
 import java.sql.Connection;
-import javax.swing.JOptionPane;
 import java.sql.ResultSet;
 import java.util.ArrayList;
 
@@ -25,19 +16,14 @@ public class ProdutosDAO {
     
     public void cadastrarProduto (ProdutosDTO produto){
         
-        
         //conn = new conectaDAO().connectDB();
-        
-        
+       
     }
     
     public ArrayList<ProdutosDTO> listarProdutos(){
         
         return listagem;
     }
-    
-    
-    
-        
+          
 }
 
--- src/DAO/conectaDAO.java
+++ src/DAO/conectaDAO.java
@@ -1,35 +1,74 @@
 package DAO;
 
 
+import Controller.ProdutosDTO;
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
-import javax.swing.JOptionPane;
 
-
-
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
-
-/**
- *
- * @author Adm
- */
 public class conectaDAO {
     
-    public Connection connectDB(){
-        Connection conn = null;
-        
+    Connection conn;
+    PreparedStatement st;
+    ResultSet rs;
+    
+    String url = "jdbc:mysql://localhost:3306/casaleilao";
+    String user = "root";
+    String pass = "admin";
+    
+    public boolean conectar(){
         try {
-        
-            conn = DriverManager.getConnection("jdbc:mysql://localhost/uc11?user=root&password=");
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            conn = DriverManager.getConnection(url,user, pass);
+            return true;
             
-        } catch (SQLException erro){
-            JOptionPane.showMessageDialog(null, "Erro ConectaDAO" + erro.getMessage());
+        } catch (ClassNotFoundException | SQLException ex) {
+            System.out.println("Erro ao conectar: " + ex.getMessage());
+            return false;
+            
         }
-        return conn;
     }
+    
+    public void desconectar(){
+            try {
+                conn.close();
+
+            } catch (SQLException ex) {
+                //pode-se deixar vazio para evitar uma mensagem de erro desnecessária ao usuário
+            }
+        }
+    
+    public int salvar (ProdutosDTO produtos){
+
+            int status;
+
+            try {
+                st = conn.prepareStatement("INSERT INTO produtos VALUES(?,?,?,?)");
+                String query = "SELECT MAX(id) FROM produtos";
+
+                ResultSet resultSet = st.executeQuery(query);
+
+                int lastId = 0;
+                if (resultSet.next()) {
+                    lastId = resultSet.getInt(1);
+                }
+                int nextId = lastId + 1;
+
+                st.setInt(1, nextId);
+                st.setString(2, produtos.getNome());
+                st.setInt(3, produtos.getValor());
+                st.setString(4, produtos.getStatus());
+
+                status = st.executeUpdate();
+                return status; //retornar 1
+
+            } catch (SQLException ex) {
+                System.out.println("Erro ao conectar: " + ex.getMessage());
+                return ex.getErrorCode();
+
+            }
+        }
     
 }
